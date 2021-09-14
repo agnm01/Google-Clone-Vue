d61cec6e22c1dc2d120f886d5293c07278b09c74
AzureAD\GabrielNegreanu <gabriel.negreanu@principal33.com>
2021-09-14 09:20:14 +0300

    Created Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <!--Footer-->
+    <footer class="footer">
+        <div>
+            <h3>Romania</h3>
+        </div>
+        <div class="footer-a">
+             <ul>
+                <li>About</li>
+                <li>Advertising</li>
+                <li>Bussines</li>
+                <li>How Search Works</li>
+            </ul>
+            <ul>
+                <li>Privacy</li>
+                <li>Terms</li>
+                <li>Settings</li>
+            </ul>
+        </div>
+    </footer>
+</template>
+
+<script>
+</script>
+
+<style>
+@import './styles/footer.css';
+</style>
